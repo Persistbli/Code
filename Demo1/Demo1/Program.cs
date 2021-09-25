27bdeb1925ccace6629a3782d319725797259b72
李大宝 <309743062@qq.com>
2021-09-25 22:24:32 +0800

    冒泡排序

---
+++ Demo1/Demo1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //冒泡排序
+            int[] a = new int[7] {3,1,5,7,9,11,6};
+            for (int j = 0; j < a.Length - 1; j++)
+            {
+                //比较两个元素大小
+                if (a[j] > a[j + 1])
+                {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+            //遍历数组中的元素
+            foreach (int b in a)
+            {
+                Console.Write( b + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
