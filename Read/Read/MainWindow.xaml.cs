b658eadff8cf2d163af41c2bfe320f3579f08ceb
bli <309743062@qq.com>
2022-09-07 18:22:51 +0800

    add Read

---
+++ Read/Read/MainWindow.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Read
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public string BookUrl { get; set; } = "https://m.xbiquge.so/book/9109/";
+        public List<string> html = new List<string>();
+        public string NextPageUrl
+        {
+            get
+            {
+                return NextPageUrl;
+            }
+            set
+            {
+                string data = ReadHelper.getDataFromUrl(value);
+                string content = ReadHelper.GetMainContent(data);
+                Application.Current.Dispatcher.Invoke(delegate () { TBContent.Text = content; Data = data; });
+            }
+        }
+        public string stringContainhtml { get; set; }
+        public bool isNextPage { get; set; } = false;
+        public string Data
+        {
+            get
+            {
+                return Data;
+            }
+            set
+            {
+                List<string> tags = new List<string>();
+                string tagName = "a";
+                tags = ReadHelper.GetTags(value, tagName);
+                IEnumerable<string> next = tags.Where((tag) => { return tag.Contains("pb_next"); }); 
+                IEnumerable<string> pre = tags.Where((tag) => { return tag.Contains("pb_next"); });
+                foreach (var VARIABLE in next)
+                {
+                    html.Add(VARIABLE);
+                }
+                foreach (var VARIABLE in pre)
+                {
+                    html.Add(VARIABLE);
+                }
+            }
+        }
+
+        private void Windows_loaded(object sender, RoutedEventArgs e)
+        {
+            //Thread th = new Thread(GetWebContent);
+            //th.IsBackground = true;
+            //th.Start();
+            if (isNextPage)
+            {
+                string data = ReadHelper.getDataFromUrl(NextPageUrl);
+                string content = ReadHelper.GetMainContent(data);
+                Application.Current.Dispatcher.Invoke(delegate () { TBContent.Text = content; Data = data; });
+            }
+            else
+            {
+                //string url = "https://m.xbiquge.so/book/9109/6516857.html";
+                string url = "https://m.xbiquge.so/book/9109/10856777.html";
+                string data = ReadHelper.getDataFromUrl(url);
+                string content = ReadHelper.GetMainContent(data);
+                Application.Current.Dispatcher.Invoke(delegate () { TBContent.Text = content; Data = data; });
+            }
+        }
+
+        private void GetWebContent()
+        {
+            while (true)
+            {
+                if (isNextPage)
+                {
+                    string data = ReadHelper.getDataFromUrl(NextPageUrl);
+                    string content = ReadHelper.GetMainContent(data);
+                    Application.Current.Dispatcher.Invoke(delegate () { TBContent.Text = content; Data = data; });
+                }
+                else
+                {
+                    string url = "https://m.xbiquge.so/book/9109/6516857.html";
+                    string data = ReadHelper.getDataFromUrl(url);
+                    string content = ReadHelper.GetMainContent(data);
+                    Application.Current.Dispatcher.Invoke(delegate () { TBContent.Text = content; Data = data; });
+                }
+                
+                
+                Thread.Sleep(5000);
+            }
+        }
+
+        private void TBContent_Changed(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void BtnNextPage_Clickes(object sender, RoutedEventArgs e)
+        {
+            foreach (var str in html)
+            {
+                string reg = @"<a[^>]*href=([""'])?(?<href>[^'""]+)\1[^>]*>";
+                MatchCollection matches = Regex.Matches(str, reg, RegexOptions.IgnoreCase);
+                foreach (Match item in matches)
+                {
+                    stringContainhtml = item.Groups[2].Value;
+                }
+            }
+            NextPageUrl = BookUrl + stringContainhtml;
+            isNextPage = true;
+        }
+
+        private void TBContent_MouseRightDown(object sender, MouseButtonEventArgs e)
+        {
+            scrollview.ScrollToTop();
+        }
+
+        private void TBContent_PreviewMouseRightDown(object sender, MouseButtonEventArgs e)
+        {
+            scrollview.ScrollToTop();
+        }
+    }
+}
--- bliexercise/bliexercise/Program.cs
+++ bliexercise/bliexercise/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Grpc.Core;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,19 +13,91 @@ namespace bliexercise
     {
         static void Main(string[] args)
         {
-            int[] nums = { 1, 2, 3, 4 };
-            int[] b = RunningSum(nums);
-
-            string Title = "《活着》";
-            string Author = "余华";
-            int year = 2001;
-            InterfaceTraing ex = new InterfaceTraing(Title,Author,year);
-            Console.WriteLine(ex.ColumnValues[0]);
-            Console.WriteLine(ex.ColumnValues[1]);
-            Console.WriteLine(ex.ColumnValues[2]);
+            //HTTP获取网页内容
+            //GetWebContent();
+            string url = "https://m.xbiquge.so/book/9109/6516857.html";
+            string data = Helper.getDataFromUrl(url);
+            string content = Helper.GetMainContent(data);
+            List<string> tag = new List<string>();
+            tag = Helper.GetTags(data , "td");
+            Console.WriteLine(content);
+            //Console.WriteLine(tag);
+            //继承的使用
+            //Rect rect = new Rect(4,5);
+            //rect.GetCost();
+            //Console.WriteLine("面积是：{0}", rect.GetArea());
+            //Console.WriteLine("成本是：{0}", rect.GetCost());
             Console.ReadKey();
-
         }
+
+        class Shape
+        {
+            public int weight { get; set; }
+            public int height { get; set; }
+            public Shape(int h,int w)
+            {
+                weight = w;
+                height = h;
+            }
+            public void SetWeight(int w)
+            {
+                weight = w;
+            }
+            public void SetHeight(int h)
+            {
+                height = h;
+            }
+
+            public int GetArea()
+            {
+                Console.WriteLine("面积是：{0}", height*weight);
+                return weight * height;
+            }
+        }
+        interface CountCost
+        {
+            int GetCost();
+        }
+
+        class Rect : Shape, CountCost
+        {
+            public Rect(int w, int h): base(w , h)
+            {
+            }
+            public int GetCost()
+            {
+                Console.WriteLine("成本为：{0}", GetArea()*50);
+                return GetArea() * 50;
+            }
+        }
+
+
+        //interface test
+        public void InterfaceTest()
+        {
+            try
+            {
+                //Channel channel = new Channel($"{SCUManager.SCUHost}:{SCUManager.GrpcPort}", ChannelCredentials.Insecure);
+                //var client = new RemoteControl.RemoteControlClient(channel);
+                //return client;
+                int[] nums = { 1, 2, 3, 4 };
+                int[] b = RunningSum(nums);
+
+                string Title = "《活着》";
+                string Author = "余华";
+                int year = 2001;
+                InterfaceTraing ex = new InterfaceTraing(Title, Author, year);
+                Console.WriteLine(ex.ColumnValues[0]);
+                Console.WriteLine(ex.ColumnValues[1]);
+                Console.WriteLine(ex.ColumnValues[2]);
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
         //动态求和
         //输入：nums = [1,1,1,1,1]
         //输出：[1,2,3,4,5]
@@ -49,6 +124,27 @@ namespace bliexercise
             else
             {
                 return false;
+            }
+        }
+        public static void GetWebContent()
+        {
+            try
+            {
+                WebClient client = new WebClient();
+                client.Credentials = CredentialCache.DefaultCredentials;
+                Byte[] webData = client.DownloadData("https://m.xbiquge.so/book/9109/6516857.html");
+                string pageHtml = Encoding.Default.GetString(webData);
+                Console.WriteLine(pageHtml);
+                using (StreamWriter sw = new StreamWriter("d:\\Doc\\ouput.html"))//将获取的内容写入文本
+                {
+                    sw.Write(pageHtml);
+                }
+                Console.ReadKey();
+            }
+            catch (Exception)
+            {
+
+                throw;
             }
         }
     }
